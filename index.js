c1f565417da90bd119efb2ea74eb0893c4bd1e2b
tonedoan <71735545+tonedoan@users.noreply.github.com>
2024-07-22 12:43:33 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count++
+    countEl.textContent = count
+}
+
+let saveEl = document.getElementById("save-el")
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    count = 0;
+    countEl.textContent = 0;
+}
+
